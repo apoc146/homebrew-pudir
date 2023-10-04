d30deada0d7c48ce29e6912b724d708feb260766
Shivam <shivambhat02@gmail.com>
2023-10-03 20:57:41 -0400

    Version 1.0 main

--- pudir.rb
+++ pudir.rb
@@ -1,8 +1,7 @@
-
 class Pudir < Formula
     desc "CLI for Purdue Directory - 'https://www.purdue.edu/directory/'"
-    homepage "https://github.com/apoc146/PUDirectory.git"
-    url "https://github.com/apoc146/PUDirectory/archive/refs/tags/v1.0.tar.gz"
+    homepage "https://github.com/apoc146/pudir.git"
+    url "https://github.com/apoc146/pudir/archive/refs/tags/v1.2.tar.gz"
     sha256 "869877e5275effe4d78ed3743d4a78c6c2286f943e7bef4539a832490ad043bc"
     license "MIT"
   
